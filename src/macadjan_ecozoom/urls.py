c89360e382fca58fb79b9808b98824cb8207320e
Andrés Moya <andres.moya@kaleidos.net>
2012-07-19 20:43:40 +0200

    Restructure templates and others

--- src/macadjan_ecozoom/urls.py
+++ src/macadjan_ecozoom/urls.py
@@ -11,5 +11,6 @@ urlpatterns = patterns('',
     url(r'^entities-kml/$', EcozoomEntitiesKml.as_view(), name='entities-kml'),
     url(r'^entities-georss/$', EcozoomEntitiesGeoRSS.as_view(), name='entities-georss'),
     url(r'^entity/(?P<entity_slug>[a-zA-Z0-9_\-]+)/$', EcozoomEntity.as_view(), name='entity'),
+    url(r'^map-source/(?P<map_source_slug>[a-zA-Z0-9_\-]+)/$', MapSourceView.as_view(), name='map-source'),
 )
 
--- src/macadjan_ecozoom/views.py
+++ src/macadjan_ecozoom/views.py
@@ -1,8 +1,12 @@
 # Create your views here.
 
+from django.views.generic import TemplateView
+from django.shortcuts import get_object_or_404
+
 from macadjan.views import EntitiesText, EntitiesList, Entity
 from macadjan.views import EntitiesKml, EntitiesGeoRSS
-from .models import EcozoomEntity
+
+from .models import EcozoomEntity, MapSource
 
 class EcozoomEntitiesText(EntitiesText):
     model = EcozoomEntity
@@ -18,4 +22,17 @@ class EcozoomEntitiesGeoRSS(EntitiesGeoRSS):
 
 class EcozoomEntity(Entity):
     model = EcozoomEntity
+
+class MapSourceView(TemplateView):
+    '''Shows detailed info about one map source'''
+    template_name = 'macadjan_ecozoom/map_source.html'
+
+    def get_context_data(self, **kwargs):
+        map_source = get_object_or_404(MapSource, slug = kwargs.get('map_source_slug', ''))
+        return {'map_source': map_source}
+
+    #def get(self, request, map_source_slug):
+    #    map_source = get_object_or_404(MapSource, slug = map_source_slug)
+    #    return render_to_response('macadjan_ecozoom/map_source.html',
+    #        {'map_source': map_source}, context_instance = RequestContext(request))
 
