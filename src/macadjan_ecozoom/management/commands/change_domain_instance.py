c7600b01b67531a6a02e1b5101bd2179cb86a83d
Andrés Moya <andres.moya@kaleidos.net>
2012-12-01 19:15:25 +0100

    Bugfix

--- src/macadjan_ecozoom/management/commands/change_domain_instance.py
+++ src/macadjan_ecozoom/management/commands/change_domain_instance.py
@@ -2,9 +2,6 @@ from django.core.management.base import BaseCommand, CommandError
 from django.utils import translation
 from django.conf import settings
 from django.contrib.sites.models import Site
-from macadjan import models
-from macadjan_ecozoom import models as models_ecozoom
-from treemenus import models as models_menus
 
 class Command(BaseCommand):
     args = '<new_domain>'
@@ -22,10 +19,11 @@ class Command(BaseCommand):
 
         translation.activate(settings.LANGUAGE_CODE)
 
-        current_site = self.init_current_site()
+        current_site = Site.objects.get_current()
         current_site.name = new_domain
         current_site.domain = new_domain
         current_site.save()
 
         translation.deactivate()
 
+        print 'Changed domain to %s' % new_domain
